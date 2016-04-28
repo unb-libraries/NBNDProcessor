837ab70b42128040c7dd7f4d6ddb05756274c3c5
Jacob Sanford <jsanford@unb.ca>
2016-04-28 15:08:28 -0300

    Process in parallel

--- processIssues.sh
+++ processIssues.sh
@@ -6,6 +6,7 @@ ISSUE_COUNTER=1
 find $INPUT_PATH -name 'metadata.php' | while read METADATA_FILE; do
   ISSUE_PATH="$OUTPUT_PATH/issue$ISSUE_COUNTER"
   mkdir -p $ISSUE_PATH
-  php -f processIssueFiles.php $METADATA_FILE $ISSUE_PATH
+  sem -j-2 php -f processIssueFiles.php $METADATA_FILE $ISSUE_PATH
   ISSUE_COUNTER=$((ISSUE_COUNTER+1))
 done
+sem --wait
