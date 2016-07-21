294a9e486c9c44fba378dffa2c333de6efeba62b
Brian Cassidy <brian.cassidy@unb.ca>
2016-07-21 12:31:14 -0300

    add a bit of info about issues queued and processed

--- processIssues.sh
+++ processIssues.sh
@@ -6,7 +6,8 @@ ISSUE_COUNTER=1
 find $INPUT_PATH -name 'metadata.php' | while read METADATA_FILE; do
   ISSUE_PATH="$OUTPUT_PATH/issue$ISSUE_COUNTER"
   mkdir -p $ISSUE_PATH
-  sem -j-2 php -f processIssueFiles.php $METADATA_FILE $ISSUE_PATH
+  echo "[INFO] QUEUED ISSUE $ISSUE_COUNTER"
+  sem -j -2 "php -f processIssueFiles.php $METADATA_FILE $ISSUE_PATH; echo [INFO] PROCESSED ISSUE $ISSUE_COUNTER"
   ISSUE_COUNTER=$((ISSUE_COUNTER+1))
 done
 sem --wait
